3a333171653341c270617beb6597453b9153ea2e
licilin <229589561@qq.com>
2020-04-16 18:04:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# audit-python-demo
+# 谛听AI智能内容审核
 botsmart 智能审核python代码示例
---
+++ audit_client_python.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import uuid
+import time
+import json
+import hashlib
+import requests
+import threading
+
+"""
+运行前请修改 APP_ID,APP_KEY,BUSINESS_ID 为对应申请到的值
+"""
+
+API_URL = 'https://api.botsmart.cn/v1/check/send'
+APP_ID = '请输入您的appId'
+APP_KEY = '请输入您的appKey'
+BUSINESS_ID = '请输入您的businesID'
+
+def sign(param):
+    list_param = []
+    for item in sorted(param.items()):
+        if item[0] == 'signature' or not item[1]:
+            continue
+        list_param.append(item[0] + '=' + str(item[1]))
+
+    return hashlib.sha1(('&'.join(list_param) + APP_KEY).encode(encoding='utf-8')).hexdigest()
+
+def sed(business_id, content):
+    param = {
+        'app_id': APP_ID,
+        'business_id': business_id,
+        'timestamp': str(int(time.time() * 1000)),
+        'unique_id': str(uuid.uuid4()),
+        'data': content,
+        'signature': ''
+    }
+    param['signature'] = sign(param)
+    # print(json.dumps(param))
+
+    response = requests.post(url=URL, data=param)
+    print(response.text)
+
+content = '请输入您要审核的内容'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
